82c717c4a60f0a22411260846dde3eff52a26e61
df-ftw <darwish.franco@ftwfoundation.org>
2025-09-29 21:16:58 +0800

    change data type of date

--- act.2-OULAD-data-modeling/dbt/transforms/grp3_oulad/models/clean/stg_oulad_assessment_grp3.sql
+++ act.2-OULAD-data-modeling/dbt/transforms/grp3_oulad/models/clean/stg_oulad_assessment_grp3.sql
@@ -4,7 +4,7 @@
 select
 	cast(id_assessment as Nullable(Int64)) as assessment_id,
 	cast(assessment_type as Nullable(Varchar(10))) as assessment_type,
-	cast(date as Nullable(Int64)) as assessment_date,
+	replace(cast(date as Nullable(String)), '?', '') as assessment_date
 	cast(weight as Nullable(Int64)) as assessment_weight,
 	cast(code_module as Nullable(Varchar(4))) as code_module,
 	cast(code_presentation as Nullable(Varchar(5))) as code_presentation
